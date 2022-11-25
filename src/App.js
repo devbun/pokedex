9ecee6f263c6f8eb02382e554494e5f1e150a35a
jakep85 <jake.p85@gmail.com>
2022-11-25 11:05:48 -0800

    testing async fetch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
 
+// async function fetchPokemon(name) {
+//   try {
+//     const endpoint = new URL(`https://pokeapi.co/api/v2/pokemon/${name}`);
+//     const response = await fetch(endpoint);
+//     const data = await response.json();
+//     console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// class Pokemon {
+//   constructor(name, image) {
+//     this.name = name;
+//     this.image = image;
+//   }
+// }
+
+// const fetchTheData = new Pokemon(fetchPokemon('pikachu'));
+// fetchPokemon('pikachu');
+
 function App() {
-  return <div className="App">hello gday</div>;
+  return <div className="App">{/* <h2 className="text-xl">{fetchTheData.name}</h2> */}</div>;
 }
 
 export default App;
