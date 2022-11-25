0c02fd703e57d0a40b07785d802023028f232084
jakep85 <jake.p85@gmail.com>
2022-11-25 10:24:07 -0800

    making a change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+  return <div className="App">hello gday</div>;
 }
 
 export default App;
