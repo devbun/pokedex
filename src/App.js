fd425b28a8eb16ffc386d7907f152d01656df22f
devbun <conanaleong@gmail.com>
2022-11-25 11:20:16 -0800

    Added get Pikachu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 
+const API_URL = "https://pokeapi.co/api/v2/pokemon/"
+
 // async function fetchPokemon(name) {
 //   try {
 //     const endpoint = new URL(`https://pokeapi.co/api/v2/pokemon/${name}`);
@@ -12,18 +14,34 @@ import './App.css';
 //   }
 // }
 
-// class Pokemon {
-//   constructor(name, image) {
-//     this.name = name;
-//     this.image = image;
-//   }
-// }
+class Pokemon {
+  constructor(name, image) {
+    this.name = name[0].toUpperCase() + name.substring(1);
+    this.image = image;
+  }
+}
 
-// const fetchTheData = new Pokemon(fetchPokemon('pikachu'));
+function getPokemon(name) {
+  const xmlHttpReq = new XMLHttpRequest();
+  xmlHttpReq.open("GET", API_URL + name, false); 
+  xmlHttpReq.send(null);
+  const res = JSON.parse(xmlHttpReq.responseText);
+
+return new Pokemon(res.name, res.sprites.front_default)
+}
+
+const fetchTheData = getPokemon('pikachu')
 // fetchPokemon('pikachu');
 
+console.log(fetchTheData)
+
 function App() {
-  return <div className="App">{/* <h2 className="text-xl">{fetchTheData.name}</h2> */}</div>;
+  return <div className="App"> 
+  <div>
+    <img src={fetchTheData.image} />
+  </div>
+  <h2 className="text-xl">{fetchTheData.name}</h2> 
+  </div>;
 }
 
 export default App;
